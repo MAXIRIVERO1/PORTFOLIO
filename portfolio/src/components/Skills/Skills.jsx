1e99e79bd51a50aad0dc9d6c3032882dc812e566
MAXIRIVERO1 <maximilianoa.castillorivero@gmail.com>
2024-09-07 15:49:58 -0300

    testing

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -6,39 +6,36 @@ import style from "./Skills.module.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Footer from '../Footer/Footer.jsx';
+import HeaderBack from '../HeaderBack/HeaderBack.jsx';
 
 function Skills() {
-  const settings = {
-    dots: false, 
-    infinite: true, 
-    speed: 500,
-    slidesToShow: 3, 
-    slidesToScroll: 2, 
-  };
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+    };
 
-  return (
-    <div>
-    <div className={style.container}>
-      <div className={style.background}>
-        <Link to={"/"}><button className={style.button}>BACK</button></Link>
-      </div>
-      <h1>Slide it!</h1>
-      <div>
-        <Slider {...settings} arrows={false}>
-          {skills.map((obj, index) => (
-            <div key={index} className={style.card}>
-              <div >
-              <img className={style.img} src={obj.image} alt="tech skill" />
-              </div>
-              <p className={style.name}>{obj.name}</p>
+    return (
+        <div>
+            <HeaderBack path="/"></HeaderBack>
+            <h1>Slide it!</h1>
+            <div>
+                <Slider {...settings} arrows={false}>
+                    {skills.map((obj, index) => (
+                        <div key={index} className={style.card}>
+                            <div >
+                                <img className={style.img} src={obj.image} alt="tech skill" />
+                            </div>
+                            <p className={style.name}>{obj.name}</p>
+                        </div>
+                    ))}
+                </Slider>
             </div>
-          ))}
-        </Slider>
-      </div>
-    </div>
-      <Footer className={style.footer}></Footer>
-    </div>
-  );
+            <Footer className={style.footer}></Footer>
+        </div>
+    );
 }
 
 export default Skills;
